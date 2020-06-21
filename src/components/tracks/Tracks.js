b495219d033dafc276b7d4c8790bb937fb35f79c
yogikatba <yogikatba16@gmail.com>
2020-06-21 12:26:16 +0530

    Remove clutter

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,29 +1,3 @@
-// import React,{useContext} from 'react'
-// import { Context } from '../../context'
-// import Spinner from "../layout/Spinner"
-// import Track from "../tracks/Track"
-
-// export const Tracks = () => {
-//     const [state] = useContext(Context)
-//     const { trackList, heading } = state
-
-//     if (trackList === undefined || trackList.length === 0) {
-//         console.log(trackList)
-//         return <Spinner />;
-//     } else {
-//         return (
-//         <>
-//             {/* <h3 className="text-center mb-4">{heading}</h3>
-//             <div className="row">
-//             {trackList.map(item => (
-//                 <Track key={item.track.track_id} track={item.track} />
-//             ))}
-//             </div> */}
-//             <h1>tracks loaded</h1>
-//         </>
-//         );
-//     }
-//     }
 import React, { useContext } from "react";
 import { Context } from "../../context";
 import Spinner from "../layout/Spinner";
@@ -31,7 +5,6 @@ import Track from './Track'
 
 const Tracks = () => {
   const { trackList, heading } = useContext(Context);
-  //const { track_list, heading } = state;
 
   if (trackList === undefined || trackList.length === 0) {
     return <Spinner />;
--- src/context.js
+++ src/context.js
@@ -10,25 +10,8 @@ export function Provider(props) {
         const cors = `https://cors-anywhere.herokuapp.com/`
         fetch(`${cors}http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
             .then(res=>res.json())
-            .then(r=>{
-                //console.log(r.message.body.track_list)
-                setTrackList(r.message.body.track_list)
-                //console.log(trackList)
-                })
+            .then(r=>{setTrackList(r.message.body.track_list)})
             .catch(e=>console.log(e))
-
-    //         fetch(
-    //             `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
-    //               process.env.REACT_APP_MM_KEY
-    //             }`
-    //           )
-    //           .then(r=>r.json())
-    //           .then(r => {
-    //             console.log(r.message.body.track_list);
-    //             setTrackList(r.message.body.track_list);
-    //             console.log(trackList)
-    //           })
-    //           .catch(e => console.log(e));
     }, [])
 
     return (
