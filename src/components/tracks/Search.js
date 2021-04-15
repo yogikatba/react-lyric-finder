697e206c04e496a3de04980e4776ea795edb2e98
yogikatba <yogikatba16@gmail.com>
2021-04-15 21:53:55 +0530

    Fixed cors error

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -8,7 +8,7 @@ const Search = () => {
 
   useEffect(() => {
     fetch(
-        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${
+        `https://cors-access-allow.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${
           process.env.REACT_APP_MM_KEY}`
       ).then(r => r.json())
       .then(res => {
